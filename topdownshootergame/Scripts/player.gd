d1bebe9453be3f934d51bd0b3528ea4d6c0a90f3
alstonStone <alstonstone0@gmail.com>
2025-02-19 21:31:02 -0500

    added player, movment and animations, rotations based off mouse position

---
+++ topdownshootergame/Scripts/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+var speed:int
+var screen_size:Vector2
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	position = screen_size/2
+	speed = 200
+
+func get_input():
+	var input_dir = Input.get_vector("left","right","up","down")
+	velocity = input_dir.normalized() * speed
+
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	move_and_slide()
+	
+	var mouse_pos = get_local_mouse_position()
+	var angle = snappedf(mouse_pos.angle(), PI / 4) / (PI / 4)
+	angle = wrapi(int(angle), 0, 8)
+	
+	$AnimatedSprite2D.animation = "walk"+str(angle)
+	
+	position = position.clamp(Vector2.ZERO,screen_size)
+	if velocity.length() !=0:
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.frame = 1
