27d95fe372a1d9443f1eada51b8fe3330d67b7a2
alstonStone <alstonstone0@gmail.com>
2025-02-26 21:08:11 -0500

    Basic HUD added

--- topdownshootergame/Scripts/main.gd
+++ topdownshootergame/Scripts/main.gd
@@ -1,10 +1,19 @@
 extends Node
 
+var wave : int
 var max_enemies : int
+var lives : int
 
 func _ready() -> void:
 	max_enemies = 10
+	wave = 1
+	lives = 3
+	$HUD/LivesLabel.text = "X "+str(lives)
+	$HUD/WaveLabel.text = "WAVE: "+str(wave)
+	$HUD/EnemiesLabel.text = "X "+str(max_enemies)
 
 func _on_enemy_spawner_hit_p() -> void:
+	lives -= 1
+	$HUD/LivesLabel.text = "X "+str(lives)
 	pass
 	
